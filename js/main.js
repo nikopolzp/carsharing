d9183ca98699afc7eb88fdd1a199cf32383e7c15
Ruslan P <nikopolzp@gmail.com>
2022-11-10 21:33:40 +0200

    add file

---
+++ js/main.js
@@ -0,0 +1,29 @@
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+var swiper = new Swiper(".commentsSwiper", {
+  slidesPerView: 3,
+  spaceBetween: 0,
+  slidesPerGroup: 1,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
